fdcebb1bc349afdb6303d8977242afa4534e547b
Valerie Graham <vgraham@bayviewglen.ca>
2023-09-11 11:26:45 -0400

    Homework Lesson 2

---
+++ Unit One - Introduction to Java/src/lesson2/Homework21.java
@@ -0,0 +1,13 @@
+package lesson2;
+
+public class Homework21 {
+    public static void main(String[] args) {
+        System.out.println("Question: What is the best Chicken McNugget sauce");
+        System.out.println("A) Spicy Buffalo");
+        System.out.println("B) Honey Mustard");
+        System.out.println("C) BBQ");
+        System.out.println("D) Creamy Ranch");
+        System.out.println("Correct Answer: B) Honey Mustard");
+    }
+
+}
---
+++ Unit One - Introduction to Java/src/lesson2/Homework22.java
@@ -0,0 +1,15 @@
+package lesson2;
+
+public class Homework22 {
+    public static void main(String[] args) {
+        System.out.println("Presentation: Exploring Huron-Wendat Heritage");
+        System.out.print("Aspects of Huron-");
+        System.out.println("Wendat Culture:");
+        System.out.println("- Traditional Practices");
+        System.out.println("- Art and Craftsmanship");
+        System.out.println("- Language and Storytelling");
+        System.out.println("- Community Traditions");
+        System.out.println("Date: Wednesday, November 15, 2023");
+        System.out.println("Duration: 60 minutes");
+    }
+}
