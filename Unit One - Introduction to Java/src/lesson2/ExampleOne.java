d3c7553f5220e551b540adf9d0f45213e2402341
Valerie Graham <vgraham@bayviewglen.ca>
2023-09-11 10:59:05 -0400

    Initial Commit

--- Unit One - Introduction to Java/src/lesson2/ExampleOne.java
+++ Unit One - Introduction to Java/src/lesson2/ExampleOne.java
@@ -3,6 +3,7 @@ package lesson2;
 public class ExampleOne {
     public static void main(String[] args) {
         System.out.println("Hello ICS3U!");
+        System.out.println("Goodbye ICS3U!");
 
     }
 }
