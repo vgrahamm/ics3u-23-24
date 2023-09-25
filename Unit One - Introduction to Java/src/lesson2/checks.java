3ebe4bf8263f543bcff3b73a6f413d2b5bd512fd
Valerie Graham <vgraham@bayviewglen.ca>
2023-09-25 10:18:06 -0400

    Lesson 3 Homework

---
+++ Unit One - Introduction to Java/src/lesson2/checks.java
@@ -0,0 +1,19 @@
+package lesson2;
+
+public class zzz {
+    public static void main(String[] args) {
+        int num = 75;
+        int onesDigit = num % 10;
+        int tensDigit = num / 10;
+        System.out.print(onesDigit);
+        System.out.println(tensDigit);
+
+        int a = 4;
+        int b = 5;
+        a++;
+        b++;
+        int c = a + b;
+        a -= 1;
+        System.out.println(a + c);
+    }
+}
