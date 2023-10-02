c3253deb64c5a0e0e3f946295ee61cd138aeecce
Valerie Graham <vgraham@bayviewglen.ca>
2023-10-01 21:40:48 -0400

    Lesson 5 Classwork

--- Unit One - Introduction to Java/src/lesson5/ExampleTHree.java
+++ Unit One - Introduction to Java/src/lesson5/ExampleTHree.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 
 public class ExampleTHree {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        Scanner input;
+        
+        input = new Scanner(System.in);
 
         System.out.print("Please enter a temperature in Fahrenheit: ");
-        int fahrenheit = in.nextInt();
+        int fahrenheit = input.nextInt();
 
         double celcius = ((double)fahrenheit - 32) * 5 / 9;
 
         System.out.println("The temperature in Celcius is: " + celcius);
 
-        in.close();
+        input.close();
 
     }
 }
