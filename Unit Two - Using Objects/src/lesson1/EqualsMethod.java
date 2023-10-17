b8468c5dac11884fd456d5a5aa93dca9f84c6c15
Valerie Graham <vgraham@bayviewglen.ca>
2023-10-16 21:54:42 -0400

    Unit 2 String and Equals

---
+++ Unit Two - Using Objects/src/lesson1/EqualsMethod.java
@@ -0,0 +1,14 @@
+package lesson1;
+
+public class EqualsMethod {
+    public static void main(String[] args) {
+        System.out.println(7==7);
+
+        int a = 8;
+        int b = 8;
+        int x = 9;
+
+        System.out.println(a==x);
+
+    }
+}
---
+++ Unit Two - Using Objects/src/lesson1/StringExamples.java
@@ -0,0 +1,21 @@
+package lesson1;
+
+public class StringExamples {
+    public static void main(String[] args) {
+        String s1 = new String("Alphabet Soup");        //s1 was created with a constructor
+        String s2 = ("happy");                          //s2 was created using a String Literal
+
+        int strLength = s1.length();
+        System.out.println(s1 + " has a length of " + strLength);
+        System.out.println(s2 + " has a length of " + s2.length());
+        System.out.println("Lester has a length of " + "Lester".length());
+
+        String s = "ppy";
+        System.out.println("The index of ppy in " + s + " in " + s2 + " is " + s2.indexOf(s)); 
+        /* be careful for the indexing - characters - 1 */
+        System.out.println("The index of happy in " + s2 + " is " + s + " is " + s.indexOf(s2));
+
+        String subString1 = s1.substring(5, 8);     // i want "bet"
+        System.out.println(subString1);
+    }
+}
