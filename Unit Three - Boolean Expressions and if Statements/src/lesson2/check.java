59e4012080b11959e65276aa3de728fa359ac2e8
Valerie Graham <vgraham@bayviewglen.ca>
2023-11-14 10:59:28 -0500

    progess check

---
+++ Unit Three - Boolean Expressions and if Statements/src/lesson2/check.java
@@ -0,0 +1,16 @@
+package lesson2;
+
+public class check {
+    public static void main(String[] args) {
+        boolean a = true;
+
+boolean b = true;
+
+System.out.print((b || (!a || b)) + " ");
+
+System.out.print(((!b || !a) && a) + " ");
+
+System.out.println(!(a && b) && b);
+    }
+    
+}
