faaf30748f87f8083eaa7d15b7ce9f0c05ac160d
Valerie Graham <vgraham@bayviewglen.ca>
2023-11-14 14:15:36 -0500

    edit

--- Unit Three - Boolean Expressions and if Statements/src/lesson2/check.java
+++ Unit Three - Boolean Expressions and if Statements/src/lesson2/check.java
@@ -3,14 +3,10 @@ package lesson2;
 public class check {
     public static void main(String[] args) {
         boolean a = true;
-
-boolean b = true;
-
-System.out.print((b || (!a || b)) + " ");
-
-System.out.print(((!b || !a) && a) + " ");
-
-System.out.println(!(a && b) && b);
+        boolean b = true;
+        System.out.print((b || (!a || b)) + " ");
+        System.out.print(((!b || !a) && a) + " ");
+        System.out.println(!(a && b) && b);
     }
     
 }
